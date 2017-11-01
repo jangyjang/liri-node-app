06cb8e61655ee083227811d98fef35a75c07ea52
Jang Yurai <jangyjang@gmail.com>
2017-11-01 18:36:07 -0500

    update

---
+++ keys.js
@@ -0,0 +1,8 @@
+var twitterKeys = {
+  consumer_key: 'wHFBkUT0DUkXLXIEhZs5s6PuO',
+  consumer_secret: 'd4p5J7cqGtMVaFcHNJ0BqVABIczVaVgA3YGLtkHEl7tp2cMnmD',
+  access_token_key: '924725049684348930-T7USYFYROysvERgrlPEoy5ilp2Ib0CI',
+  access_token_secret: 'Qy58NnoNMQQAwiSgUQfXWp5Dc8rDzHy41L1t9PELwBnhL',
+}
+
+module.exports = twitterKeys;
